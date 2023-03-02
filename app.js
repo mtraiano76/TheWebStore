aff6044812b271a99eb43fc5486971922de4379b
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2023-03-02 14:24:21 -0300

    Secciones

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var generoRouter = require("./routes/genero");
+var novedadesRouter = require("./routes/novedades");
+var contactoRouter = require("./routes/contacto");
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/genero", generoRouter);
+app.use("/novedades", novedadesRouter);
+app.use("/contacto", contactoRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
