035d0faa5b18c7b044db99a6ffb6a95f8c1edfa1
Miguel Traiano <92335885+mtraiano76@users.noreply.github.com>
2023-03-08 12:27:22 -0300

    Index diseño

--- app.js
+++ app.js
@@ -32,6 +32,9 @@ app.use("/novedades", novedadesRouter);
 app.use("/contacto", contactoRouter)
 
 
+app.listen (80)
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
